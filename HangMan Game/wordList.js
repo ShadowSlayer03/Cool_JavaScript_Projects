092c4ed20ce8919beda4e1d66a15973d241ab40b
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-10-01 12:57:10 +0530

    Added a resource file for main JS script

---
+++ HangMan Game/wordList.js
@@ -0,0 +1,235 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "computer",
+        hint: "An electronic device for processing data."
+    },
+    {
+        word: "ocean",
+        hint: "A vast body of saltwater."
+    },
+    {
+        word: "mountain",
+        hint: "A large landform that rises steeply above its surroundings."
+    },
+    {
+        word: "pencil",
+        hint: "An object that you write or draw with."
+    },
+    {
+        word: "painting",
+        hint: "A work of art created with paint on a surface."
+    },
+    {
+        word: "bicycle",
+        hint: "A two-wheeled vehicle powered by pedaling."
+    },
+    {
+        word: "camera",
+        hint: "A device for capturing photographs or videos."
+    },
+    {
+        word: "flower",
+        hint: "A colorful bloom often found in gardens."
+    },
+    {
+        word: "restaurant",
+        hint: "A place where food and drinks are served to customers."
+    },
+    {
+        word: "telephone",
+        hint: "A communication device for voice conversations."
+    },
+    {
+        word: "sunrise",
+        hint: "The time in the morning when the sun appears above the horizon."
+    },
+    {
+        word: "umbrella",
+        hint: "A device used to protect against rain or sun."
+    },
+    {
+        word: "jungle",
+        hint: "A dense, tropical forest with abundant vegetation."
+    },
+    {
+        word: "telescope",
+        hint: "An optical instrument for distant viewing of objects."
+    },
+    {
+        word: "wallet",
+        hint: "A small, flat case for carrying money and cards."
+    },
+    {
+        word: "fireplace",
+        hint: "A structure for containing a fire in a dwelling."
+    },
+    {
+        word: "sunglasses",
+        hint: "Eyewear designed to protect the eyes from sunlight."
+    },
+    {
+        word: "butterfly",
+        hint: "A flying insect with colorful wings."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet, often brown treat made from cacao beans."
+    },
+    {
+        word: "saxophone",
+        hint: "A member of a family of metal wind instruments with a reed like that of a clarinet, used especially in jazz and dance music."
+    },
+    {
+        word: "calculator",
+        hint: "Something used for making mathematical calculations, in particular a small electronic device with a keyboard and a visual display."
+    },
+    {
+        word: "stream",
+        hint: "A small, narrow river."
+    },
+    {
+        word: "bookshelf",
+        hint: "A piece of furniture with shelves for storing books."
+    },
+    {
+        word: "heroic",
+        hint: "Displaying courage or bravery, often in the face of danger."
+    },
+    {
+        word: "furry",
+        hint: "Covered with fur or having a furry texture."
+    },
+    {
+        word: "economical",
+        hint: "Efficient and cost-effective in the use of resources."
+    },
+    {
+        word: "spicy",
+        hint: "Having a strong, pungent, or hot flavor."
+    },
+    {
+        word: "telephone",
+        hint: "A communication device for voice conversations."
+    },
+    {
+        word: "dusk",
+        hint: "The time of day when the sun has set, but it's not completely dark yet."
+    },
+    {
+        word: "canopy",
+        hint: "An overhead covering or structure, often found in forests."
+    },
+    {
+        word: "microscope",
+        hint: "A scientific instrument used for magnifying tiny objects or organisms."
+    },
+    {
+        word: "wallet",
+        hint: "A small, flat case for carrying money and cards."
+    },
+    {
+        word: "fireplace",
+        hint: "A structure for containing a fire in a dwelling."
+    },
+    {
+        word: "sunglasses",
+        hint: "Eyewear designed to protect the eyes from sunlight."
+    },
+    {
+        word: "butterfly",
+        hint: "A flying insect with colorful wings."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet, often brown treat made from cacao beans."
+    },
+    {
+        word: "keyboard",
+        hint: "A set of keys used for typing on a computer or musical instrument."
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of stars, planets, and the universe."
+    },
+    {
+        word: "whale",
+        hint: "A large marine mammal."
+    },
+    {
+        word: "waterfall",
+        hint: "A natural cascade of water."
+    },
+    {
+        word: "dictionary",
+        hint: "A reference book containing words, their meanings, and other information."
+    },
+    {
+        word: "oasis",
+        hint: "A fertile spot in a desert where water is found, often with vegetation."
+    },
+    {
+        word: "volcano",
+        hint: "A mountain or hill with a vent where lava, rocks, and gases erupt."
+    },
+    {
+        word: "harmony",
+        hint: "A pleasing combination of musical notes or elements."
+    },
+    {
+        word: "sculpture",
+        hint: "A three-dimensional work of art created by shaping or carving a material."
+    },
+    {
+        word: "cycling",
+        hint: "The activity of riding a bicycle for exercise or leisure."
+    },
+    {
+        word: "geography",
+        hint: "The study of Earth's physical features and the distribution of life on the planet."
+    },
+    {
+        word: "skyscraper",
+        hint: "A very tall, multi-story building."
+    },
+    {
+        word: "gourmet",
+        hint: "Involving or relating to the preparation and consumption of high-quality food."
+    },
+    {
+        word: "starry",
+        hint: "Filled with stars or resembling a star."
+    },
+    {
+        word: "adventure",
+        hint: "An exciting and unusual experience or activity."
+    },
+    {
+        word: "quaint",
+        hint: "Attractively unusual or old-fashioned."
+    },
+    {
+        word: "penguin",
+        hint: "A flightless seabird native to the Southern Hemisphere."
+    },
+    {
+        word: "carousel",
+        hint: "A rotating platform with seats, often found at amusement parks."
+    },
+    {
+        word: "watermelon",
+        hint: "A juicy, sweet fruit with green skin and red or pink flesh."
+    },
+    {
+        word: "piano",
+        hint: "A large keyboard musical instrument with strings."
+    },
+    {
+        word: "robotics",
+        hint: "The branch of technology that deals with the design, construction, and operation of robots."
+    },]
+
+    
